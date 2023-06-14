714b630e45e6b630bd319df2dff37495e1acce69
sharmarahul1103 <sharma.rahul@ksolves.com>
2023-06-14 16:07:14 +0530

    added multiply feature

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -9,6 +9,7 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 		System.out.println("application started in main");
+		System.out.println("multiply feature:"+ 2*3);
 	}
 
 }
